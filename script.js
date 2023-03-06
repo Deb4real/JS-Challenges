bfece5fb1dcf75513a67d90265fe4aa5a025d7e2
Deborah Olushina <deboraholushina19@gmail.com>
2023-03-05 20:33:06 -0600

    Function 2-3

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 // 1. Write a function that takes a number and returns true if it is a positive number and false if it is a negative number
-function IsNumberPositive(-1);
+function IsNumberPositive(event) {
+    const IsNumberPositive=event.target.value;
+
+    if (IsNumberPositive>=10) {
+        return true
+    } else if (IsNumberPositive<=-1) {
+        return false
+    }  
+}
+alert(IsNumberPositive(10));
+
+// 2. Write a function that takes a number of days and converts it into an age
+function convertsDaystoAge(event) {
+    const age=(3650);
+    return age/365;
+}
+console.log(convertsDaystoAge(3650/365));
+
+// 3. Write a function that takes three numbers and returns the largest of the three numbers
+function getLargestNumber(event) {
+    const getLargestNumber=(a,b,c);
+
+    if ((a >= b) && (a >= c)) {
+        return a;
+    } else if ((b >= a) && (b >= c)) {
+        return b;
+    } else {
+        return c;
+    }
+}
+console.log(getLargestNumber(2,1,4));
